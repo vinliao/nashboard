dd1a4d4197523c297a31d64ada9ecd9ed357b5b5
Vincent Liao <vincent.liao@protonmail.com>
2022-02-24 14:29:36 +0700

    add profile.svelte

---
+++ src/routes/profile.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Tweet from '$lib/Tweet.svelte';
+	const replied = {message: "reply one", time: "2m"}
+</script>
+
+<div class="mb-5"></div>
+<Tweet
+	message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
+	time="3m"
+	pubkey="c3765...cd0076"
+	replied={replied}
+/>
+
+<Tweet
+	message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
+	time="3m"
+	pubkey="c3765...cd0076"
+	replied={replied}
+/>
+
+<Tweet
+	message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
+	time="3m"
+	pubkey="c3765...cd0076"
+	replied={replied}
+/>
+
+<Tweet
+	message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
+	time="3m"
+	pubkey="c3765...cd0076"
+/>
