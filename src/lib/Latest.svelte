22250fba9b0d19593736ea5dae9baee3a93a434b
Vincent Liao <vincent.liao@protonmail.com>
2022-02-25 17:34:14 +0700

    show latest tweets

--- src/lib/Latest.svelte
+++ src/lib/Latest.svelte
@@ -1,37 +1,19 @@
 <script>
 	import Tweet from "./Tweet.svelte";
+	export let events;
+	const latestEvents = [...new Set(events)].slice(-20).reverse();
 	const replied = {message: "reply one", time: "2m", pubkey: "54f18...8f2a9"};
 </script>
 
 <div class="rounded-md shadow border p-3">
 	<span class="block text-center pb-3 text-xs text-neutral-400">NOSTR NETWORK'S LATEST EVENTS</span>
 	<div class="flex flex-col">
-	<Tweet
-		message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
-		time="3m"
-		pubkey="c3765...cd0076"
-		replied={replied}
-	/>
-
-	<Tweet
-		message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
-		time="3m"
-		pubkey="c3765...cd0076"
-	/>
-
-	<Tweet
-		message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
-		time="3m"
-		pubkey="c3765...cd0076"
-		replied={replied}
-	/>
-
-	<Tweet
-		message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur harum suscipit perferendis inventore dolorum numquam voluptatem aperiam reiciendis nostrum vitae!"
-		time="3m"
-		pubkey="c3765...cd0076"
-		replied={replied}
-	/>
-
+	{#each latestEvents as event}
+		<Tweet
+			message={event.content}
+			time={event.created_at}
+			pubkey={event.pubkey}
+		/>
+	{/each}
   </div>
 </div>
