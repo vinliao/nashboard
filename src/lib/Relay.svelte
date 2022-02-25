0de3d292336dc95f0c924e33219dc7303e6e9f22
Vincent Liao <vincent.liao@protonmail.com>
2022-02-25 16:24:51 +0700

    implement relay activity count

--- src/lib/Relay.svelte
+++ src/lib/Relay.svelte
@@ -1,13 +1,15 @@
 <script>
+	export let relays;
+	export let relayActivity;
 </script>
 
 <div class="rounded-md shadow border p-3">
 	<span class="block text-center pb-3 text-xs text-neutral-400">EVENTS RECEIVED BY RELAY LAST 24H</span>
 	<div class="flex flex-col">
-		{#each Array(5) as _}
+		{#each relays as relay, i}
 			<div class="flex p-2 break-words">
-				<span class="flex-1 mr-3 text-neutral-500">wss://your-relay-name.com</span>
-				<span class="shrink text-neutral-400">999 evt</span>
+				<span class="flex-1 mr-3 text-neutral-500">{relay}</span>
+				<span class="shrink text-neutral-400">{relayActivity[i]}</span>
 			</div>
 		{/each}
   </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,57 +8,80 @@
 	import Unique from '$lib/Unique.svelte';
 	import Dots from '$lib/Dots.svelte';
 
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
 	let eventCount24h = 0;
 	let eventCount1h = 0;
 	let networkActivity = Array(24).fill(0); // array index is hour
 
 	const unixTime = Math.floor(Date.now() / 1000);
-	const unixTimeMinus24h = unixTime - (60 * 60 * 24);
-	const unixTimeMinus1h = unixTime - (60 * 60);
+	const unixTimeMinus24h = unixTime - 60 * 60 * 24;
+	const unixTimeMinus1h = unixTime - 60 * 60;
+
+	// taken from github.com/fiatjaf/nostr-relay-registry
+	const relays = [
+		'wss://nostr-pub.wellorder.net',
+		'wss://relayer.fiatjaf.com',
+		'wss://nostr.rocks',
+		'wss://rsslay.fiatjaf.com',
+		'wss://freedom-relay.herokuapp.com/ws',
+		'wss://nostr-relay.freeberty.net',
+		'wss://nostr.bitcoiner.social',
+		'wss://nostr-relay.wlvs.space',
+		'wss://nostr.onsats.org',
+		'wss://nostr-relay.untethr.me',
+		'wss://nostr-verified.wellorder.net',
+		'wss://nostr.drss.io',
+		'wss://nostr.unknown.place'
+	];
+	let relayActivity = Array(relays.length).fill(0);
 
 	// only do it on front end
 	onMount(() => {
-		let url = "wss://nostr-pub.wellorder.net";
-		let socket = new WebSocket(url);
+		relays.forEach((url, relayIndex) => {
+		// let url = 'wss://nostr-pub.wellorder.net';
+			let socket = new WebSocket(url);
 
-		socket.onopen = function (event) {
-			console.log(`Connected to ${url}`);
-			
-			// pull all event 24h
-			socket.send(JSON.stringify(["REQ", "foobar", {"since": unixTimeMinus24h}]));
-		};
+			socket.onopen = function (event) {
+				console.log(`Connected to ${url}`);
 
-		// // take data and parse here
-		socket.onmessage = function (incomingPayload) {
-			const payload = JSON.parse(incomingPayload.data);
-			const event = payload[2];
-			const eventDate = new Date(event.created_at * 1000);
+				// pull all event 24h
+				socket.send(JSON.stringify(['REQ', 'foobar', { since: unixTimeMinus24h }]));
+			};
 
-			// count total event
-			eventCount24h++;
-			if(event.created_at > unixTimeMinus1h) eventCount1h++;
+			// // take data and parse here
+			socket.onmessage = function (incomingPayload) {
+				const payload = JSON.parse(incomingPayload.data);
+				const event = payload[2];
+				const eventDate = new Date(event.created_at * 1000);
 
-			// count peak event
-			const eventHour = eventDate.getUTCHours();
-			networkActivity[eventHour]++;
-		}
-	})
+				// count total event
+				eventCount24h++;
+				if (event.created_at > unixTimeMinus1h) eventCount1h++;
+
+				// count peak event
+				const eventHour = eventDate.getUTCHours();
+				networkActivity[eventHour]++;
+
+				// count relay activity
+				relayActivity[relayIndex]++;
+			};
+		})
+	});
 </script>
 
 <section>
 	<div class="m-2">
-		<Count eventCount1h={eventCount1h} eventCount24h={eventCount24h}/>
-		<div class="mb-3"></div>
+		<Count {eventCount1h} {eventCount24h} />
+		<div class="mb-3" />
 		<!-- <Average/>
 		<div class="mb-3"></div> -->
-		<Activity networkActivity={networkActivity}/>
-		<div class="mb-3"></div>
-		<Relay/>
-		<Dots/>
-		<Latest/>
-		<div class="mb-3"></div>
-		<Unique/>
+		<Activity {networkActivity} />
+		<div class="mb-3" />
+		<Relay {relays} {relayActivity}/>
+		<Dots />
+		<Latest />
+		<div class="mb-3" />
+		<Unique />
 	</div>
 </section>
