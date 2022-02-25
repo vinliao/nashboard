1f8123ebfbb4030f2f90294dac698586d6f4b652
Vincent Liao <vincent.liao@protonmail.com>
2022-02-25 15:58:29 +0700

    add basic websocket capability on count.svelte

--- src/lib/Count.svelte
+++ src/lib/Count.svelte
@@ -1,12 +1,17 @@
+<script>
+	export let eventCount1h;
+	export let eventCount24h;
+</script>
+
 <div class="rounded-md shadow border p-3">
 	<span class="block text-center pb-3 text-xs text-neutral-400">NOSTR NETWORK'S EVENT COUNT</span>
 	<div class="flex">
 		<div class="flex-1 text-center">
-			<span class="text-2xl">30</span>
+			<span class="text-2xl">{eventCount1h}</span>
 			<span class="text-neutral-400">last 1h</span>
 		</div>
 		<div class="flex-1 text-center">
-			<span class="text-2xl">999</span>
+			<span class="text-2xl">{eventCount24h}</span>
 			<span class="text-neutral-400">last 24h</span>
 		</div>
 	</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,14 +7,49 @@
 	import Average from '$lib/Average.svelte';
 	import Unique from '$lib/Unique.svelte';
 	import Dots from '$lib/Dots.svelte';
+
+	import { onMount } from "svelte";
+
+	let eventCount24h = 0;
+	let eventCount1h = 0;
+	let networkActivity = Array(24).fill(0); // array index is hour
+
+	const unixTime = Math.floor(Date.now() / 1000);
+	const unixTimeMinus24h = unixTime - (60 * 60 * 24);
+	const unixTimeMinus1h = unixTime - (60 * 60);
+
+	// only do it on front end
+	onMount(() => {
+		let url = "wss://nostr-pub.wellorder.net";
+		let socket = new WebSocket(url);
+
+		socket.onopen = function (event) {
+			console.log(`Connected to ${url}`);
+			
+			// pull all event 24h
+			socket.send(JSON.stringify(["REQ", "foobar", {"since": unixTimeMinus24h}]));
+		};
+
+		// // take data and parse here
+		socket.onmessage = function (incomingPayload) {
+			let payload = JSON.parse(incomingPayload.data);
+			let event = payload[2];
+
+			// count total event
+			eventCount24h++;
+			if(event.created_at > unixTimeMinus1h) eventCount1h++;
+
+			// count peak event
+		}
+	})
 </script>
 
 <section>
 	<div class="m-2">
-		<Count/>
+		<Count eventCount1h={eventCount1h} eventCount24h={eventCount24h}/>
 		<div class="mb-3"></div>
-		<Average/>
-		<div class="mb-3"></div>
+		<!-- <Average/>
+		<div class="mb-3"></div> -->
 		<Activity/>
 		<div class="mb-3"></div>
 		<Relay/>
