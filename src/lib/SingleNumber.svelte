210bf6108715b87aa92e42154e5e21b0e97596c3
Vincent Liao <vincent.liao@protonmail.com>
2022-08-21 19:13:20 +0700

    make count value tween

--- src/lib/SingleNumber.svelte
+++ src/lib/SingleNumber.svelte
@@ -1,13 +1,24 @@
 <script>
   export let number;
   export let label;
+  import { tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
+
+  const progress = tweened(0, {
+    duration: 1500,
+    easing: cubicOut,
+  });
+
+
+  progress.set(number);
 </script>
 
 <div class="p-5 border-2 border-orange-200 bg-white">
-  <span class="block text-center pb-1 text-lg text-orange-500 font-bold tracking-tighter"
+  <span
+    class="block text-center pb-1 text-lg text-orange-500 font-bold tracking-tighter"
     >{label}</span
   >
   <div class="text-center">
-    <span class="text-4xl text-stone-700 tracking-[-0.075em]">{number}</span>
+    <span class="text-4xl text-stone-700 tracking-[-0.075em]">{Math.floor($progress)}</span>
   </div>
 </div>
