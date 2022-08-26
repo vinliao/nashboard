a36ac7b8a097e7d25965cc81651f3a71990f7733
Vincent Liao <vincent.liao@protonmail.com>
2022-08-26 16:41:17 +0700

    make icon darker

--- src/lib/TweetList.svelte
+++ src/lib/TweetList.svelte
@@ -172,7 +172,7 @@
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          class="w-5 h-5"
+          class="w-5 h-5 text-stone-600"
         >
           <path
             fill-rule="evenodd"
