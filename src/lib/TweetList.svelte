7b08c176f032860806ccc2a9464089564695c649
Vincent Liao <vincent.liao@protonmail.com>
2022-08-26 15:50:16 +0700

    refresh button gets latest events

--- src/lib/TweetList.svelte
+++ src/lib/TweetList.svelte
@@ -8,13 +8,44 @@
   export let foundIn = [];
 
   let extended = false;
+
+  async function getRecentEvents() {
+    tweets = [];
+    extendedTweets = [];
+    await fetch("/api/data/insert/events"); // fill redis cache
+    const body = await (await fetch("/api/data/get/events")).json();
+
+    tweets = body.events.slice(0, 8); // 8 is shortListAmount
+    extendedTweets = body.events;
+  }
 </script>
 
 <div class="p-5 border-2 border-orange-200 bg-white xl:min-w-0 xl:basis-1/2">
-  <span
-    class="block text-center text-lg text-orange-500 font-bold tracking-tighter"
-    >LATEST EVENTS</span
-  >
+  <div class="flex items-center">
+    <div class="flex-1" />
+    <span
+      class="block text-center text-lg text-orange-500 font-bold tracking-tighter"
+      >LATEST EVENTS</span
+    >
+    <div class="flex-1 flex">
+      <div class="flex-1" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 text-stone-500 hover:cursor-pointer hover:text-stone-700 transition"
+        on:click={getRecentEvents}
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M4.5 12c0-1.232.046-2.453.138-3.662a4.006 4.006 0 013.7-3.7 48.678 48.678 0 017.324 0 4.006 4.006 0 013.7 3.7c.017.22.032.441.046.662M4.5 12l-3-3m3 3l3-3m12 3c0 1.232-.046 2.453-.138 3.662a4.006 4.006 0 01-3.7 3.7 48.657 48.657 0 01-7.324 0 4.006 4.006 0 01-3.7-3.7c-.017-.22-.032-.441-.046-.662M19.5 12l-3 3m3-3l3 3"
+        />
+      </svg>
+    </div>
+  </div>
 
   <div class="flex flex-col">
     {#each tweets as tweet, tweetIndex}
