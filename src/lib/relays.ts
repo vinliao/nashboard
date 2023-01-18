5b06fcdd142753c1dc6e8e40ca4e57a8f34ca1a4
Jack Chakany <jackchakany@outlook.com>
2023-01-17 23:44:35 -0500

    Update relays.ts (#11)

--- src/lib/relays.ts
+++ src/lib/relays.ts
@@ -34,5 +34,6 @@ export const relayList: RelayInterface[] = [
   { name: "wss://nostr-relay.nonce.academy", alias: 'nonce.academy' },
   { name: "wss://relay.realsearch.cc", alias: 'realsearch.cc' },
   { name: "wss://nostr.developer.li", alias: 'developer.li' },
+  { name: "wss://nostr.chaker.net", alias: 'chaker' },
   { name: "wss://relay.nostr.scot", alias: 'nostr.scot' }
 ];
